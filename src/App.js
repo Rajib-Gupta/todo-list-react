93c6c8425a63ff7c94cb22e0dcdbb603808e6806
Rajib Gupta <84308590+Rajib-Gupta@users.noreply.github.com>
2022-01-26 20:05:28 +0530

    bugs fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {Header} from './header/header';
 import Todos from "./todo/todos";
--- src/todo/AddTodo.js
+++ src/todo/AddTodo.js
@@ -14,11 +14,11 @@ export default function AddTodo(props) {
         <h1>Add a Todo</h1>
         <form onSubmit={submit}>
             <div className="mb-3">
-                <label for="exampleInputEmail1" className="form-label">Title</label>
+                <label htmlFor="exampleInputEmail1" className="form-label">Title</label>
                 <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
             </div>
             <div className="mb-3">
-                <label for="exampleInputPassword1" className="form-label">Description</label>
+                <label htmlFor="exampleInputPassword1" className="form-label">Description</label>
                 <input type="text" value={desc} onChange={(e)=>setdesc(e.target.value)} className="form-control" id="exampleInputPassword1" />
             </div>
             <button type="submit" className="btn btn-primary">Submit</button>
